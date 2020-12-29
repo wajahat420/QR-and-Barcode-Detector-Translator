6ed8e6bfade393df95370245f3c9c6078d5f08f5
wajahat420 <shayanmustafa11@gmail.com>
2020-12-29 00:15:53 +0000

    changes in pdf to image converter module in code_test.py

--- code_test.py
+++ code_test.py
@@ -11,42 +11,43 @@ import csv
 import argparse
 import datetime
 
+from pdf2image import convert_from_path, convert_from_bytes
+import fitz
+
 
 def convertBase64ToPdf(pdfURL):
     comma = pdfURL.find(",")
     pdfURL = pdfURL[comma+1:]
-
+    print("\n =>",pdfURL[:40],"\n")
     with open('generated.pdf', 'wb') as theFile:
         theFile.write(base64.b64decode(pdfURL))
+
 
 def scan_with_pdf():
     workbook = xlwt.Workbook()
     sheet = workbook.add_sheet("Sheet 1")
     style = xlwt.easyxf('font: bold 1')
 
-    pdffile = open('generated.pdf', 'rb')
-    doc = minecart.Document(pdffile)
-        
     ap = argparse.ArgumentParser()
     ap.add_argument("-o", "--output", type=str, default="barcodes_pdfs.csv",
                     help="Path to output csv file")
     args = vars(ap.parse_args())
     csv = open(args["output"], "w")
-    page = doc.get_page(0)  # getting a single page
+    # page = doc.get_page(0)  # getting a single page
     csv.write("{},{},{}\n".format("Date", "Barcode Data","Barcode Type"))
 
-    count = 0
-    # iterating through all pages
-    for page in doc.iter_pages():
-        im = page.images[0].as_pil()  # requires pillow
-        count = count + 1
-        name = str(count) + '.jpg'
-        im.save(name)
+        
+    pages = convert_from_path('my.pdf', 500)
 
-    # time.sleep(4)
+    count = 0
+    for page in pages:
+        count += 1
+        page.save(str(count)+'.jpg', 'JPEG')
+
 
     for x in range(count):
         image = cv2.imread(str(x+1)+".jpg")
+        # image = cv2.imread("my.png")
         barcodes = decode(image)
         print("decoded=> ",len(barcodes))
 
@@ -75,3 +76,61 @@ def scan_with_pdf():
     # if cv2.waitKey(0):
     #     cv2.destroyAllWindows()
         # break
+# scan_with_pdf()
+
+
+# pdffile = open('2222.pdf', 'rb')
+# doc = minecart.Document(pdffile)
+
+# count = 0
+# for page in doc.iter_pages():
+#     print("checking",page.images[0],"\n",page.images[0].as_pil())
+#     im = page.images[0].as_pil()  # requires pillow
+#     count = count + 1
+#     name = str(count) + '.jpg'
+#     im.save(name)
+
+
+# def pdf2jpeg(pdf_input_path, jpeg_output_path):
+# args = ["pef2jpeg", # actual value doesn't matter
+#         "-dNOPAUSE",
+#         "-sDEVICE=jpeg",
+#         "-r144",
+#         "-sOutputFile=" + "/home/wajahat/Desktop/qr-and-barcode-detector-translator/",
+#         "b.pdf"]
+
+# encoding = locale.getpreferredencoding()
+# args = [a.encode(encoding) for a in args]
+
+# ghostscript.Ghostscript(*args)
+
+# import tempfile
+# from pdf2image import convert_from_path, convert_from_bytes
+
+
+# from pdf2image import convert_from_path
+# pages = convert_from_path('2222.pdf', 500)
+
+# count = 1
+# for page in pages:
+#     page.save(str(count)+'.jpg', 'JPEG')
+#     count += 1
+
+
+# doc = fitz.open("b.pdf")
+# count = 0
+# for i in range(len(doc)):
+#     for img in doc.getPageImageList(i):
+#         xref = img[0]
+#         pix = fitz.Pixmap(doc, xref)
+#         print("length",xref,img,"\n",pix)
+#         if pix.n < 5:       # this is GRAY or RGB
+#             count += 1
+#             pix.writePNG("%s.png" % (count)) 
+#         else:               # CMYK: convert to RGB first
+#             count += 1
+#             pix1 = fitz.Pixmap(fitz.csRGB, pix)
+#             pix1.writePNG("%s.png" % (count))
+#             pix1 = None
+#         pix = None
+
